56684bd6b2ec7b85f5c15ff604133e20c28dff9a
Aaron Scherer <aequasi@gmail.com>
2020-02-19 11:47:23 -0800

    Updating output on no config

--- src/util/get-config.ts
+++ src/util/get-config.ts
@@ -1,7 +1,7 @@
 import EnvPath from 'env-paths'
-import fs from 'fs'
+import fs from 'fs-extra'
 import path from 'path'
-import downloadUrlToFile from './download-url'
+import downloadUrlToFile, {downloadUrl} from './download-url'
 
 const packageJson = require('../../package.json')
 const configPath  = EnvPath(packageJson.name).config // eslint-disable-line new-cap
@@ -21,35 +21,39 @@ interface Config {
 
 type ConfigFn = (config: {version: string; repo: string}) => Config;
 
-export function getLocalConfig(repo: string, version: string): ConfigFn | undefined {
+export async function getLocalConfig(repo: string, version: string): Promise<ConfigFn | undefined> {
   const pth = path.join(configPath, repo)
 
-  if (fs.existsSync(path.join(process.cwd(), '.repro.js'))) {
+  if (await fs.pathExists(path.join(process.cwd(), '.repro.js'))) {
     return require(path.join(process.cwd(), '.repro.js'))
   }
 
-  if (fs.existsSync(path.join(pth, version + '.js'))) {
-    return require(path.join(pth, version + '.js'))
+  if (await fs.pathExists(path.join(pth, version, '.repro.js'))) {
+    return require(path.join(pth, version, '.repro.js'))
   }
 }
 
 export async function getRemoteConfig(repo: string, version: string): Promise<void> {
   const vrs = version === 'latest' ? 'master' : version
 
-  await downloadUrlToFile(
-    `https://raw.githubusercontent.com/${repo}/${vrs}/.repro.js`,
-    path.join(configPath, repo, version + '.js')
-  )
+  await fs.mkdirp(path.join(configPath, repo, version));
+  const content = await downloadUrl(`https://raw.githubusercontent.com/${repo}/${vrs}/.repro.js`);
+  if (!content.startsWith('404: Not Found')) {
+    await fs.writeFile(path.join(configPath, repo, version, '.repro.js'), content);
+  }
 }
 
 export default async function getConfig(repo: string, version = 'latest'): Promise<Config | undefined> {
-  let config = getLocalConfig(repo, version)
+  let config = await getLocalConfig(repo, version)
   if (!config) {
     await getRemoteConfig(repo, version)
-    config = getLocalConfig(repo, version)
+    config = await getLocalConfig(repo, version)
   }
 
   if (!config) {
+    const pth = path.join(configPath, repo)
+    console.log('Create your own config by creating a .repro.js file in: ' + path.join(pth, version) + '/\n')
+
     throw new Error('Config not found for ' + repo + '@' + version)
   }
 
